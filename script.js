a4880a75e0665dd5aced7d9daa5d275e54702370
LucaGiona <luca@senzproduction.com>
2025-07-24 08:14:22 +0200

    toggle title page

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-fetch("/templates/nav.html")
-    .then(res => res.text())
-    .then(data => {
-        document.getElementById("nav-container").innerHTML = data;
-    })
+//Titel in den Metadaten
+ 
+  const titles = [
+    "test",
+    "Maranta – Willkommen",
+    "Webentwickler & Bar Manager",
+    "Coole Ideen"
+  ];
+
+  let i = 0;
+  setInterval(() => {
+    document.title = titles[i % titles.length];
+    i++;
+  }, 1000); // alle 3 Sekunden wechseln
+
